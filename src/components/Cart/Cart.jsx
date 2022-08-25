1e88920ffc3e18550a167604266ade821ef3dac0
Sameh <sameh.abdelhaleem95@gmail.com>
2022-08-25 22:02:53 +0200

    add Cart component

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,12 @@
+import classes from "./Cart.module.css";
+const Cart = (props) => {
+  return (
+    <div>
+      <div>cartitems</div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Cart;
