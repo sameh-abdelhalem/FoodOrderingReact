ed72b69695cf067cdc213a3b92171dff240b61d4
Sameh <sameh.abdelhaleem95@gmail.com>
2022-08-26 19:43:43 +0200

    create Cart Component

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,10 +1,23 @@
 import classes from "./Cart.module.css";
 const Cart = (props) => {
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
   return (
     <div>
-      <div>cartitems</div>
-      <div></div>
-      <div></div>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>35.62</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes["button"]}>order</button>
+      </div>
     </div>
   );
 };
