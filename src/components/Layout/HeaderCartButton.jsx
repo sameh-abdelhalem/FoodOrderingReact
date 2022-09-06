30d77c6cf28146e642ecf5327ddebf6748055aea
Sameh <sameh.abdelhaleem95@gmail.com>
2022-09-06 22:02:15 +0200

    implement bump animation in HeaderCartButton

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,15 +1,33 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [btnOn, setBtnOn] = useState(false);
   const cartCtx = useContext(CartContext);
   const numOfCartItems = cartCtx.items.reduce((curNum, item) => {
     return curNum + item.amount;
   }, 0);
+
+  const btnClasses = `${
+    btnOn ? `${classes.button} ${classes.bump}` : classes.button
+  }`;
+  const { items } = cartCtx;
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return;
+    }
+    setBtnOn(true);
+    const timer = setTimeout(() => {
+      setBtnOn(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
   return (
-    <button className={classes.button} onClick={props.showCart}>
+    <button className={btnClasses} onClick={props.showCart}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
